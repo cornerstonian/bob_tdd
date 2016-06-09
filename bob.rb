049ede1f4f2eef72be2611bfcb46d4ee4f330615
cornerstonian <vois.cornerstone@gmail.com>
2016-06-08 23:52:34 -0500

    20 runs, 13 assertions, 2 failures, 0 errors, 7 skips

--- bob.rb
+++ bob.rb
@@ -1,2 +1,15 @@
 class Bob
+
+  def hey(remark)
+    if remark == remark.upcase
+      'Whoa, chill out!'
+    elsif remark.end_with?("?")
+        'Sure.'
+    elsif remark.strip == ''
+        'Fine. Be that way!'
+    else
+      'Whatever.'
+    end
+  end
+
 end
--- bob_test.rb
+++ bob_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+
 require_relative 'bob'
 
 class BobTest < Minitest::Test
@@ -16,19 +17,16 @@ class BobTest < Minitest::Test
   end
 
   def test_shouting
-    skip
     remark = 'WATCH OUT!'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_gibberish
-    skip
     remark = ('A'..'Z').to_a.shuffle[0, 10].join
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_asking_a_question
-    skip
     remark = 'Does this cryogenic chamber make me look fat?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
@@ -40,31 +38,26 @@ class BobTest < Minitest::Test
   end
 
   def test_asking_gibberish
-    skip
     remark = ('a'..'z').to_a.shuffle[0, 10].join << '?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_talking_forcefully
-    skip
     remark = "Let's go make out behind the gym!"
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_using_acronyms_in_regular_speech
-    skip
     remark = "It's OK if you don't want to go to the DMV."
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_forceful_questions
-    skip
     remark = 'WHAT THE HELL WERE YOU THINKING?'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_numbers
-    skip
     remark = '1, 2, 3 GO!'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
@@ -88,7 +81,6 @@ class BobTest < Minitest::Test
   end
 
   def test_shouting_with_no_exclamation_mark
-    skip
     remark = 'I HATE YOU'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
@@ -106,13 +98,11 @@ class BobTest < Minitest::Test
   end
 
   def test_silence
-    skip
     remark = ''
     assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
   end
 
   def test_prolonged_silence
-    skip
     remark = ' ' * rand(1..10)
     assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
   end
@@ -124,7 +114,6 @@ class BobTest < Minitest::Test
   end
 
   def test_on_multiple_line_questions
-    skip
     remark = %(
 Does this cryogenic chamber make me look fat?
 no)
