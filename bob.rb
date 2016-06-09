6994a4515aefae028383306a548a9900c3ab4e54
cornerstonian <vois.cornerstone@gmail.com>
2016-06-09 15:53:24 -0500

    all tests passing

--- bob.rb
+++ bob.rb
@@ -1,15 +1,27 @@
 class Bob
 
   def hey(remark)
-    if remark == remark.upcase
-      'Whoa, chill out!'
+
+    if remark.strip == ""
+      'Fine. Be that way!'
+    elsif   remark == remark.upcase && (remark =~ /[A-Za-z]/)
+      "Whoa, chill out!"
     elsif remark.end_with?("?")
         'Sure.'
-    elsif remark.strip == ''
-        'Fine. Be that way!'
     else
       'Whatever.'
     end
   end
 
 end
+
+
+
+
+
+
+# Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+# Bob answers 'Sure.' if you ask him a question.
+# He answers 'Whoa, chill out!' if you yell at him.
+# He says 'Fine. Be that way!' if you address him without actually saying anything.
+# He answers 'Whatever.' to anything else. Instructions
